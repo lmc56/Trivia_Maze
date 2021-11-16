f84c95d54dd26b34f251eb3ed4669f81c1d2d6cc
Andrew Moreno-Escareno <andrewmoreno355@gmail.com>
2021-11-15 23:46:29 -0800

    -m

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -2,5 +2,24 @@ package com.company;
 import java.sql.*;
 public class Room {
 
+    int num;
+    private String question;
+    private String answer;
+    boolean result;
 
+    public Room(){
+
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public boolean isResult() {
+        return result;
+    }
 }
