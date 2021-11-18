3cf77c39eb64ecb88fbf63727978fe1b71503874
Andrew Moreno-Escareno <andrewmoreno355@gmail.com>
2021-11-17 23:17:26 -0800

    -m

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Room basic = new Room();
-        System.out.println(basic.getQuestion());
+
        // Maze pract = new Maze(4, 4);
         //System.out.println("hello");
     }
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,18 +1,26 @@
 package com.company;
 import java.sql.*;
+import java.util.Scanner;
+
 public class Room {
 
-    int num;
-    private String question;
-    private String answer;
-    boolean result;
+//    int num;
+//    private String question;
+//    private String answer;
+//    boolean result;
+    Scanner in = new Scanner(System.in);
     String url = "jdbc:sqlite:C:/Users/andre/Trivia_Maze/src/com/company/new_file";
 
-    public Room(){
 
+    public Room(){
+        getQuestion();
+        getAnswer();
+        System.out.println(isResult());
     }
 
-    public String getQuestion() {
+
+
+    public void getQuestion() {
         String question = "";
         try{
             Connection con = DriverManager.getConnection(url);
@@ -25,15 +33,31 @@ public class Room {
             System.err.println(e.getMessage());
         }
 
-        return question;
+        System.out.println(question);
     }
 
     public String getAnswer() {
+        String answer = "";
+
+        try{
+            Connection con = DriverManager.getConnection(url);
+            Statement statement = con.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from QUESTIONS");
+            answer = resultSet.getString(2);
+        }
+        catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
 
         return answer;
     }
 
     public boolean isResult() {
+        String myAnswer = in.nextLine();
+
+        if (myAnswer.equals(getAnswer()))
+            return true;
 
         return false;
     }
