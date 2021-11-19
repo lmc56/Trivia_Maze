86440b6029e9ed4905e5d668adc9fa9033ef82d6
Andrew Moreno-Escareno <andrewmoreno355@gmail.com>
2021-11-18 20:47:56 -0800

    finished room class

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -4,61 +4,32 @@ import java.util.Scanner;
 
 public class Room {
 
-//    int num;
-//    private String question;
-//    private String answer;
-//    boolean result;
     Scanner in = new Scanner(System.in);
     String url = "jdbc:sqlite:C:/Users/andre/Trivia_Maze/src/com/company/new_file";
 
 
     public Room(){
-        getQuestion();
-        getAnswer();
-        System.out.println(isResult());
-    }
-
-
-
-    public void getQuestion() {
         String question = "";
-        try{
-            Connection con = DriverManager.getConnection(url);
-            Statement statement = con.createStatement();
-
-            ResultSet resultSet = statement.executeQuery("select * from QUESTIONS");
-            question = resultSet.getString(1);
-        }
-        catch(SQLException e){
-            System.err.println(e.getMessage());
-        }
-
-        System.out.println(question);
-    }
-
-    public String getAnswer() {
         String answer = "";
-
         try{
             Connection con = DriverManager.getConnection(url);
             Statement statement = con.createStatement();
 
-            ResultSet resultSet = statement.executeQuery("select * from QUESTIONS");
+            ResultSet resultSet = statement.executeQuery("select * from QUESTIONS ORDER BY random() LIMIT 1");
+            question = resultSet.getString(1);
             answer = resultSet.getString(2);
         }
         catch(SQLException e){
             System.err.println(e.getMessage());
         }
 
-        return answer;
+        System.out.println(question);
+        System.out.println(isResult(answer));
     }
 
-    public boolean isResult() {
+    public boolean isResult(String answer) {
         String myAnswer = in.nextLine();
 
-        if (myAnswer.equals(getAnswer()))
-            return true;
-
-        return false;
+        return myAnswer.equals(answer);
     }
 }
