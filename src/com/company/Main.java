59f2fbf86230865755bb3938e2c48c682b62a5dc
Levi McCoy (lmc56) <lmc56@uw.edu>
2021-11-22 22:53:59 -0800

    added new files to branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Room basic = new Room();
 
        // Maze pract = new Maze(4, 4);
         //System.out.println("hello");
 	// write your code here
         System.out.println("test");
         Menu myMenu = new Menu();
+        Room basic = new Room();
     }
 }
