4d48facbb37cc0ce58e97667a07db6b5a7fb9513
Andrew Moreno-Escareno <andrewmoreno355@gmail.com>
2021-11-23 21:51:39 -0800

    reformatting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Room basic = new Room();
+        Door basic = new Door();
 
        // Maze pract = new Maze(4, 4);
         //System.out.println("hello");
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,63 +1,7 @@
 package com.company;
-import javax.swing.*;
-import java.util.*;
-import java.sql.*;
+
 
 public class Room {
 
-    String url = "jdbc:sqlite:C:/Users/andre/Trivia_Maze/src/com/company/new_file";
 
-
-    public Room(){
-        String question = "";
-        String answer = "";
-        String a1 = "";
-        String a2 = "";
-
-        try{
-            Connection con = DriverManager.getConnection(url);
-            Statement statement = con.createStatement();
-
-            ResultSet resultSet = statement.executeQuery("select * from QUESTIONS ORDER BY random() LIMIT 1");
-            question = resultSet.getString(1);
-            answer = resultSet.getString("ANSWER");
-            a1 = resultSet.getString("A1");
-            a2 = resultSet.getString("A2");
-
-        }
-        catch(SQLException e){
-            System.err.println(e.getMessage());
-        }
-
-        //Code obtained from javadoc oracle
-        // (https://docs.oracle.com/javase/7/docs/api/javax/swing/JOptionPane.html)
-        Object[] possibleAnswers = getAnswerSet(new String[]{answer, a1, a2}, answer);
-        Random text = new Random();
-        int n = text.nextInt(possibleAnswers.length - 1);
-
-        Object selectedValue = JOptionPane.showInputDialog(null,
-                question, "Room",
-                JOptionPane.INFORMATION_MESSAGE, null,
-                possibleAnswers, possibleAnswers[n]);
-
-        System.out.println(question);
-        System.out.println(isResult(answer,(String) selectedValue));
-    }
-
-    public boolean isResult(String answer,String myAnswer) {
-        return myAnswer.equals(answer);
-    }
-
-    public Object[] getAnswerSet(String[] ans, String a){
-        if (a.equals("T") || a.equals("F")){
-            ans = new String[]{"T", "F"};
-            return ans;
-        }
-
-        List<String> strList = Arrays.asList(ans);
-        Collections.shuffle(strList);
-        ans = strList.toArray(new String[strList.size()]);
-
-        return ans;
-    }
 }
