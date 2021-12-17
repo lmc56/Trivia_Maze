0333e7971b97658787c68d2fb3bce877f39bf01d
Andrew Moreno-Escareno <andrewmoreno355@gmail.com>
2021-12-17 15:02:47 -0800

    reformatting

--- src/com/company/DoorTest.java
+++ src/com/company/DoorTest.java
@@ -4,22 +4,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DoorTest {
     Door testDoor = new Door();
+    String testQuestion;
+    String testAnswer;
 
     @org.junit.jupiter.api.Test
     void getQuestion() {
-
+        assertEquals(testDoor.question,testDoor.getQuestion());
     }
 
     @org.junit.jupiter.api.Test
     void getAnswer() {
+        assertEquals(testDoor.answer, testDoor.getAnswer());
     }
 
     @org.junit.jupiter.api.Test
     void getA1() {
+        assertEquals(testDoor.a1, testDoor.getA1());
     }
 
     @org.junit.jupiter.api.Test
     void getA2() {
+        assertEquals(testDoor.a2, testDoor.getA2());
     }
 
     @org.junit.jupiter.api.Test
