03699ba5c14191467109948a773f1f839d676117
Andrew Moreno-Escareno <andrewmoreno355@gmail.com>
2021-12-17 03:03:31 -0800

    reformatting

---
+++ src/com/company/DoorTest.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoorTest {
+    Door testDoor = new Door();
+
+    @org.junit.jupiter.api.Test
+    void getQuestion() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAnswer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getA1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getA2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isResult() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAnswerSet() {
+    }
+}
